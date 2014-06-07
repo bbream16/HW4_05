61b819c39de9f3b7696578d16f6f3b67554b518f
bbream16 <bbream16@my.whitworth.edu>
2014-06-07 06:44:51 -0700

    HW4_05

---
+++ HW4_05/Course.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Course.h"
+using namespace std;
+
+
+
+int* doubleCapacity(const int* list, int size){
+	int new_size = size * 2;
+	int* new_array = new int[new_size];
+
+
+
+	return new_array;
+}
+string thisCourse::getCourseName() const
+{
+
+}
+void thisCourse::addStudent(const string& name)
+{
+
+}
+void thisCourse::dropStudent(const string& name)
+{
+
+}
+string* thisCourse::getStudents() const
+{
+
+}
+int thisCourse::getNumberOfStudents() const
+{
+
+}
+void thisCourse::clear()
+{
+
+}
+
+
+int main(){
+
+	int list[6] = { 1, 2, 3, 4, 5, 6 };
+	int* p = doubleCapacity(p, 6);
+
+
+	for (int i = 0; i < 10; ++i)
+		cout << p[i] << endl;
+
+	delete[] p;
+
+	Course thisCourse(const string& ComputerScience, int 30);
+	thisCourse.dropStudent(const string Bob);
+}
